80cfca5fb92ef57a70f85432ac826d3c13fe4087
David Euijoon Kim <dk.davidekim@gmail.com>
2025-02-18 20:36:18 -0500

    Update formTemplate.sh

--- Homework 1c/Transforming and Staging/formTemplate.sh
+++ Homework 1c/Transforming and Staging/formTemplate.sh
@@ -4,9 +4,9 @@
 random_string1=$(echo $RANDOM | md5sum | cut -c 1-6)
 random_string2=$(echo $RANDOM | md5sum | cut -c 1-6)
 
-# Concatenate "db" with the 6-character random string plus "-DS310db" to create a unique database name
-DBNAME="db${random_string1}-DS310db"
-DBSERVERNAME="db${random_string2}-ds310server"
+# Concatenate "db" with the 6-character random string plus "-MOD4db" to create a unique database name
+DBNAME="db${random_string1}-MOD4db"
+DBSERVERNAME="db${random_string2}-MOD4server"
 
 # Use double quotes around variable substitutions to ensure any spaces/special characters are handled correctly
 sed -i -e "s/@dbName/$DBNAME/g" ./template/parameters.json
