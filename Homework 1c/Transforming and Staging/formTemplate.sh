2b361ab8c85f22f0da4fb1e2a25c7591653bd355
David Euijoon Kim <dk.davidekim@gmail.com>
2025-10-13 12:58:08 -0400

    Update formTemplate.sh

--- Homework 1c/Transforming and Staging/formTemplate.sh
+++ Homework 1c/Transforming and Staging/formTemplate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Set the subscription to 'Azure for Students'
+az account set --subscription "Azure for Students"
+
+# Extract the 'id' field using jq from the output of az account show
+subId=$(az account show | jq -r '.id')
+
+# If you still want to allow manual entry, you can check if the above command was successful and prompt the user if not.
+if [[ -z "$subId" ]]; then
+    read -p "Enter subscription id manually: " subId
+fi
+
 # Get a 6-character random string
 random_string1=$(echo $RANDOM | md5sum | cut -c 1-6)
 random_string2=$(echo $RANDOM | md5sum | cut -c 1-6)
